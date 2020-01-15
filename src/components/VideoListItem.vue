0637aef6d30144dc1870e8b97e88509d7c9af1be
KewinLizarraga <kewin392@gmail.com>
2020-01-15 17:02:13 -0500

    [ADD] list item

---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- <a :href="'https://www.youtube.com/watch?v='+video.id.videoId" target="_blank">
+      <img class="card-img-left" :src="video.snippet.thumbnails.medium.url" alt="miniatura YouTube" />
+    </a>-->
+    <div class="card-body">
+      <h5 class="card-title">
+        <a
+          :href="'https://www.youtube.com/watch?v=' + video.id.videoId"
+          class="card-link"
+          target="_blank"
+        >{{ video.snippet.title }}</a>
+      </h5>
+      <!-- <h5 class="card-title">{{video.snippet.title}}</h5> -->
+      <h6
+        class="card-subtitle"
+      >{{video.snippet.channelTitle}} | {{video.snippet.publishedAt || formatDate}}</h6>
+      <p class="card-text mb-2 text-muted">{{video.snippet.description}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoList",
+  props: ["video"]
+};
+</script>
