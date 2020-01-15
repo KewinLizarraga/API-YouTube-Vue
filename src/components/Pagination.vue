92d634ecf3e64b058c67c1a5b89fa51e043e5820
KewinLizarraga <kewin392@gmail.com>
2020-01-15 17:02:34 -0500

    [ADD] pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <nav>
+      <ul class="pagination justify-content-end">
+        <li class="page-item" v-bind:class="{ disabled: prevPageToken === undefined }">
+          <a class="page-link" href="#" aria-label="Previous" @click="prevPage">
+            <span aria-hidden="true">&laquo; Previous</span>
+          </a>
+        </li>
+
+        <li
+            class="page-item"
+            v-bind:class="{ disabled: nextPageToken === undefined }"
+        >
+          <a class="page-link" href="#" aria-label="Next" @click="nextPage">
+            <span aria-hidden="true">Next &raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagination",
+  methods: {
+    prevPage() {
+      this.$emit("prev-page");
+    },
+    nextPage() {
+      this.$emit("next-page");
+    }
+  },
+  props: ["prevPageToken", "nextPageToken"]
+};
+</script>
+
+<style scoped>
+.page-link {
+  box-shadow: none !important;
+}
+</style>
